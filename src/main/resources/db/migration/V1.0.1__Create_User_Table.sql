621eb9ac24ef31641c2217527a028373c30e3af7
Viktor Modroczky <v.modroczky@gmail.com>
2022-03-11 16:55:42 +0100

    migration changes

--- src/main/resources/db/migration/V1.0.1__Create_User_Table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    email VARCHAR UNIQUE NOT NULL,
-    password VARCHAR NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
-);
